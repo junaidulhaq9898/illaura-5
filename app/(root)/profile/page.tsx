a2f5e1945f2199e39202140d225a5f0100f31b80
junaid <junaid@Junaids-MacBook-Pro.local>
2024-12-07 02:01:58 +0530

    Fix authentication middleware and module resolution errors, update dependencies, and resolve hydration issue

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,6 +1,9 @@
 import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import { redirect } from "next/navigation";
+
+import { Collection } from "@/components/shared/Collection";
+import Header from "@/components/shared/Header";
 import { getUserImages } from "@/lib/actions/image.actions";
 import { getUserById } from "@/lib/actions/user.actions";
 
