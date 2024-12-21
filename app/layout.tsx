250f5fa6e0728782c99dc6274fc4a40e24a21dda
junaid <junaid@Junaids-MacBook-Pro.local>
2024-12-21 23:11:40 +0530

    Fix linting and TypeScript errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
@@ -7,8 +9,8 @@ import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-ibm-plex'
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex"
 });
 
 export const metadata: Metadata = {
@@ -22,9 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{
-      variables: { colorPrimary: '#624cf5' }
-    }}>
+    <ClerkProvider 
+      appearance={{
+        variables: { colorPrimary: "#624cf5" },
+      }}
+    >
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
